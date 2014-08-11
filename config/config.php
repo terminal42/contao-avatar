16029827f612cc2acdaf5cfb52d0f674badbde82
Yanick Witschi <yanick.witschi@terminal42.ch>
2014-08-11 13:24:30 +0200

    Fixed default config was not applied

--- config/config.php
+++ config/config.php
@@ -24,3 +24,15 @@ $GLOBALS['TL_FFL']['avatar'] = 'FormAvatar';
  */
 $GLOBALS['TL_HOOKS']['executePostActions'][] = array('Avatar', 'dispatchAjaxRequest');
 $GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('Avatar', 'replaceInsertTags');
+
+/**
+ * Config defaults
+ */
+$GLOBALS['TL_CONFIG']['avatar_member_size']         = 'a:2:{i:0;s:3:"100";i:1;s:3:"100";}';
+$GLOBALS['TL_CONFIG']['avatar_member_thumb']        = 'a:2:{i:0;s:3:"160";i:1;s:3:"160";}';
+$GLOBALS['TL_CONFIG']['avatar_member_extensions']   = 'jpg,jpeg,gif,png';
+$GLOBALS['TL_CONFIG']['avatar_member_maxlength']    = 500000;
+$GLOBALS['TL_CONFIG']['avatar_user_size']           = 'a:2:{i:0;s:3:"100";i:1;s:3:"100";}';
+$GLOBALS['TL_CONFIG']['avatar_user_thumb']          = 'a:2:{i:0;s:3:"160";i:1;s:3:"160";}';
+$GLOBALS['TL_CONFIG']['avatar_user_extensions']     = 'jpg,jpeg,gif,png';
+$GLOBALS['TL_CONFIG']['avatar_user_maxlength']      = 500000;
